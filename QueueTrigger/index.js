6a71c40640e1f4bedd3a4e1ee1d403a3a8f5754d
Alexis Mateo <alexismateod@outlook.com>
2020-08-13 12:39:25 -0400

    intital

---
+++ QueueTrigger/index.js
@@ -0,0 +1,32 @@
+
+var helper = require('sendgrid').mail;
+var sendgrid = require('sendgrid')(process.env.SENDGRID_API_KEY);
+
+
+module.exports = async function (context, myQueueItem) {
+
+        var fromEmail = new helper.Email('alexismateod@outlook.com');
+        var toEmail = new helper.Email(myQueueItem);
+        var subject = 'Welcome!!!';
+        var content = new helper.Content('text/plain', 'Welcome to the familly');
+        var mail = new helper.Mail(fromEmail, subject, toEmail, content);
+    
+
+        var request = sendgrid.emptyRequest({
+            method: 'POST',
+            path: '/v3/mail/send',
+            body: mail.toJSON()
+        });
+
+        sendgrid.API(request, function (error, response) {
+            if (error) {
+              console.log('Error response received');
+            }
+            console.log(response.statusCode);
+            console.log(response.body);
+            console.log(response.headers);
+          });    
+
+        console.log(request);
+
+};
