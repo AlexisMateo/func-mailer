1ed0dbe9acb5224e4612cc2a4dfe502439f7c588
Alexis Mateo <alexismateod@outlook.com>
2020-08-13 12:42:57 -0400

    [fix]: function.json

--- QueueTrigger/index.js
+++ QueueTrigger/index.js
@@ -5,10 +5,10 @@ var sendgrid = require('sendgrid')(process.env.SENDGRID_API_KEY);
 
 module.exports = async function (context, myQueueItem) {
 
-        var fromEmail = new helper.Email('alexismateod@outlook.com');
+        var fromEmail = new helper.Email(process.env.EMAIL_SENDER);
         var toEmail = new helper.Email(myQueueItem);
         var subject = 'Welcome!!!';
-        var content = new helper.Content('text/plain', 'Welcome to the familly');
+        var content = new helper.Content('text/plain', 'Welcome to the familly ' + myQueueItem);
         var mail = new helper.Mail(fromEmail, subject, toEmail, content);
     
 
